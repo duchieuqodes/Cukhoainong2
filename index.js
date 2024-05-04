10d8ea05427d5dd2c67c9f09d340e99a6cac50ff
duchieuqodes <168545152+duchieuqodes@users.noreply.github.com>
2024-05-04 21:42:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const mongoose = require('mongoose');
 const cron = require('node-cron'); // Thư viện để thiết lập cron jobs
+const keep_alive = require('./keep_alive.js')
 
 // Kết nối tới MongoDB
 mongoose.connect(
